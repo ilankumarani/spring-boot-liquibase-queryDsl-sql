a599de4d21caebac790cfb84f214ce2d14422c3c
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2025-06-21 21:42:46 +0100

    Refactoring

--- liquibase-app/src/test/java/com/ilan/MultiTest.java
+++ liquibase-app/src/test/java/com/ilan/MultiTest.java
@@ -1,10 +1,11 @@
 package com.ilan;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-
+@Disabled
 @ActiveProfiles("multi")
 @SpringBootTest
 class MultiTest {
